9c69b8675b2d723b8aafe4375a99883603adbb15
GyeongTak Byeon <bkt1214bkt@naver.com>
2020-12-02 16:06:59 +0900

    2번째 커밋. 경탁 작업화면들

---
+++ PlaceTab/app/src/main/java/com/example/placetab/Favorite.java
@@ -0,0 +1,15 @@
+package com.example.placetab;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+
+
+public class Favorite extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_favorite);
+    }
+
+}
